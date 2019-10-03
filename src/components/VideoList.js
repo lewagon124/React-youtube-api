be50e63b08002f2b09392cd29addbc3bec01a392
Seong Park <eslteach124@gmail.com>
2019-10-03 11:56:58 -0400

    Youtube

---
+++ src/components/VideoList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Grid} from '@material-ui/core';
+import VideoItem from './VideoItem';
+
+const VideoList = ({videos, onVideoSelect}) =>{
+  const listofVideos = videos.map((video, id)=><VideoItem onVideoSelect={onSelectVideo} key={id} video={video}/>)
+  return (
+    <Grid container spacing={10}>
+      {listofVideos}
+    </Grid>
+    );
+}
+
+export default VideoList;
